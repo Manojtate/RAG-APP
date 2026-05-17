88fc55bc5c41feb79184481fc5ea3b91fe444019
Manoj Tate <manojtate@gmail.com>
2026-05-17 18:13:23 +0530

    Initial commit

---
+++ src/main/java/com/spring/ai/firstProject/first_project/controller/ChatController.java
@@ -0,0 +1,29 @@
+package com.spring.ai.firstProject.first_project.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ChatController {
+
+
+    private ChatClient chatClient;
+
+    public  ChatController(ChatClient.Builder builder)
+    {
+         this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(
+            @RequestParam (value = "q") String  query)
+    {
+        var ResponseContent = this.chatClient.prompt(query).call().content();
+          return  ResponseEntity.ok(ResponseContent);
+    }
+}
