b5f9a6888a6b4812692df8c16a5e475e5c884154
Manoj Tate <manojtate@gmail.com>
2026-05-21 23:58:24 +0530

    learned user useUserPrompt

--- src/main/java/com/spring/ai/firstProject/first_project/controller/ChatController.java
+++ src/main/java/com/spring/ai/firstProject/first_project/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.spring.ai.firstProject.first_project.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +15,35 @@ public class ChatController {
 
     private ChatClient chatClient;
 
-    public  ChatController(ChatClient.Builder builder)
-    {
-         this.chatClient = builder.build();
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
     }
 
     @GetMapping("/chat")
     public ResponseEntity<String> chat(
-            @RequestParam (value = "q") String  query)
-    {
+            @RequestParam(value = "q") String query) {
         var ResponseContent = this.chatClient.prompt(query).call().content();
-          return  ResponseEntity.ok(ResponseContent);
+        return ResponseEntity.ok(ResponseContent);
     }
+
+    @GetMapping("/user/chat")
+    public ResponseEntity<String> useUserPrompt(
+            @RequestParam(value = "q") String query) {
+        Prompt prompt = new Prompt(query);
+        String strQuery = "As an expert in coding always write code in java .Now replay for this question :{query}";
+
+//        var userSpec = PromptUserSpec.builder()
+//                .text(strQuery)
+//                .param("query", query);
+//
+//        chatClient.prompt().user(userSpec);
+        var ResponseContent = this.chatClient.
+                prompt().user(u -> u.text(strQuery).param("query", query))
+                .call().content();
+
+
+        return ResponseEntity.ok(ResponseContent);
+    }
+
+
 }
